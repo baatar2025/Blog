19c66b6d1962cbb5e264a741a38b6aaa3aac6aff
Baatar Chuluunbat <23LP0037@G304-CC0084.local>
2024-01-12 17:56:18 +0800

    2024_1_12

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
-import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
-import { useState,useEffect } from 'react'
-import { Articles } from '@/components/Articles'
 import { Nav } from '@/components/Nav'
 import { Trending } from '@/components/trend/Trending'
 import { Allpost } from '@/components/allpost/Allpost'
 import { Footer } from '@/components/Footer'
-import { Usereff } from './Usereff'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +12,6 @@ export default function Home() {
   return (
     <main className=''>
       <div className=''>
-        {/* <Usereff/> */}
        <Nav/>
        <Trending/>
        <Allpost/>
